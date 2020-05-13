20e61ccc9cea7adb151cce0eadcef6d596d38d00
Andaru Pramudito Suhud <6870663+aps2201@users.noreply.github.com>
2020-05-13 09:16:37 +0700

    add analytics template

---
+++ analytics_template.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from apiclient.discovery import build
+import google.auth
+import pandas as pd
+from pandas import DataFrame as df
+import re
+import datetime as dt
+
+
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file(
+    "SERVICE_ACCOUNT_KEY_JSON") # add service account key file here
+credentials = credentials.with_scopes(['https://www.googleapis.com/auth/analytics.readonly'])
+
+analytics = build('analyticsreporting', 'v4', credentials=credentials)
+VIEW_ID='VIEW_ID' # view id you want to pull from your google analytics account
+
+
+class report:
+    def build_report(analytics,view_id,start_date,end_date,metrics=[],dimensions=[]): # create a query to build report
+        return analytics.reports().batchGet(
+            body={
+                "reportRequests":
+                [
+                    {
+                        "viewId":view_id,
+                        "dateRanges": [{"startDate": start_date, "endDate": end_date}],
+                        "metrics": metrics,
+                        "dimensions": dimensions,
+                        "samplingLevel":  "LARGE"
+                    }
+                ]
+            })
+    def get_report(report_build): # pull the report based on the report build
+        return report_build.execute()
+    
+    def ga_to_df(ga): # google analytics report from get_report()
+        # split into dicts
+        kv=ga.get('reports')[0] 
+        
+        # get headers
+        dimensions=[re.sub('ga:','',dim) for dim in kv.get('columnHeader').get('dimensions')] # list comprehension, re.sub each element and make list        
+        metric_headers=kv.get('columnHeader').get('metricHeader').get('metricHeaderEntries')
+        metrics=[]
+        for metrics_ in metric_headers:
+            metrics.append(re.sub('ga:','',metrics_['name']))
+        
+        # get data, dimensions labels and metric values
+        data=kv.get('data').get('rows') 
+        labels=[]
+        for dimension_labels in data:
+            labels.append(dimension_labels.get('dimensions'))
+            values=[]
+            for metric_values in data:
+                for values_ in metric_values.get('metrics'):
+                    values.append(values_.get('values'))
+        
+        # dataframing    
+        values=df.from_dict(values)
+        values=values.rename(columns=dict(zip(values.columns.to_list(),metrics)))
+        
+        labels=df.from_dict(labels)
+        labels=labels.rename(columns=dict(zip(labels.columns.to_list(),dimensions)))
+        
+        x=pd.concat([labels,values],axis=1)
+        return x
