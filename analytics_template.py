74d93e38872f6811de786eebea47b9d227780aef
Andaru Pramudito Suhud <6870663+aps2201@users.noreply.github.com>
2020-05-14 10:52:15 +0700

    fix indent

--- analytics_template.py
+++ analytics_template.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[4]:
+
 
 from apiclient.discovery import build
 import google.auth
@@ -10,14 +12,20 @@ import re
 import datetime as dt
 
 
+# In[5]:
+
+
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_file(
-    "SERVICE_ACCOUNT_KEY_JSON") # add service account key file here
+    'apsweb-07ed3c8c3585.json')
 credentials = credentials.with_scopes(['https://www.googleapis.com/auth/analytics.readonly'])
 
 analytics = build('analyticsreporting', 'v4', credentials=credentials)
-VIEW_ID='VIEW_ID' # view id you want to pull from your google analytics account
+VIEW_ID='136660919'
+
+
+# In[10]:
 
 
 class report:
@@ -35,7 +43,7 @@ class report:
                     }
                 ]
             })
-    def get_report(report_build): # pull the report based on the report build
+    def get_report(report_build):
         return report_build.execute()
     
     def ga_to_df(ga): # google analytics report from get_report()
@@ -55,9 +63,9 @@ class report:
         for dimension_labels in data:
             labels.append(dimension_labels.get('dimensions'))
             values=[]
-            for metric_values in data:
-                for values_ in metric_values.get('metrics'):
-                    values.append(values_.get('values'))
+        for metric_values in data:
+            for values_ in metric_values.get('metrics'):
+                values.append(values_.get('values'))
         
         # dataframing    
         values=df.from_dict(values)
@@ -68,3 +76,47 @@ class report:
         
         x=pd.concat([labels,values],axis=1)
         return x
+
+
+# In[11]:
+
+
+q = report.build_report(analytics,
+                      view_id='136660919',
+                      start_date='2020-04-01',
+                      end_date=dt.date.today().strftime('%F'),
+                      metrics=[{'expression':'ga:sessions'},{'expression':'ga:users'},{'expression':'ga:totalEvents'}],
+                      dimensions=[{'name':'ga:date'},{'name':'ga:deviceCategory'},{'name':'ga:eventCategory'},{'name':'ga:eventLabel'}]
+                     )
+
+
+# In[12]:
+
+
+ga=report.get_report(q)
+
+
+# In[13]:
+
+
+x=report.ga_to_df(ga)
+report.ga_to_df(ga)
+
+
+# In[60]:
+
+
+x.dtypes
+
+
+# In[36]:
+
+
+dt.datetime.today().strftime("%F %H%M%S")
+
+
+# In[ ]:
+
+
+
+
